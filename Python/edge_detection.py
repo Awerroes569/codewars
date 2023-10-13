bccaf23455844775a3415d6f1f2d7c40a5ceb893
Awerroes <arssoo569@gmail.com>
2023-10-13 21:53:12 +0200

    further version edge_detection

--- Python/edge_detection.py
+++ Python/edge_detection.py
@@ -12,7 +12,7 @@ def decode(rle):
             for _ in range(int(next_counts)):
                 matrix.append(next_value)
         except StopIteration:
-            ##print('iteration stopped')
+            ###print('iteration stopped')
             break
     result=[]
     while matrix:
@@ -22,7 +22,7 @@ def decode(rle):
         
 
 def to_rle(data):
-    #print(f'data: {data[0]}')
+    ##print(f'data: {data[0]}')
     column=len(data[0])
     data=data[:]
     gen_data=(y for x in data for y in x)
@@ -42,7 +42,7 @@ def to_rle(data):
             value=next_value
             next_value=next(gen_data) 
         except StopIteration:
-            #print('stop iteration')
+            ##print('stop iteration')
             result.append(value)
             result.append(count)
             break
@@ -57,7 +57,7 @@ def calculate_diff(row,column,matrix):
             r,c=direction
             if row+r>-1 and column+c>-1:
                 neighbour=int(matrix[row+r][column+c])
-                #print(f'neighbour: {neighbour}')
+                ##print(f'neighbour: {neighbour}')
                 diff=abs(value-neighbour)
                 result=max(result,diff)
         except IndexError:
@@ -72,13 +72,19 @@ def transform(matrix):
             result[row][column]=(calculate_diff(row,column,matrix))
     return result[:]
 
-def edge_detection(raw):
+def edge_detection2(raw):
     data=decode(raw)
-    print(f'after decode: {data}')
+    #print(f'after decode: {data}')
 
     transformed=transform(data)
-    print(f'after transformed: {transformed}')
+    #print(f'after transformed: {transformed}')
     return to_rle(transformed)
+def edge_detection(raw):
+    columns,data = decode2(raw)
+
+    return encode(columns, data)
+
+
 
 def decode2(data):
     data=(x for x in data.split(' '))
@@ -90,13 +96,12 @@ def decode2(data):
             value=int(next(data))
             counter=int(next(data))
             count+=counter
-            results.append((value,count))
+            results.append((count,value))
         except StopIteration:
             break
-    return results[:]
+    return (column,results[:])
 
-for item in decode2(raw2):
-    print(item)
+
 
 def detect_zero(data, columns):
     rows=data[1]//columns
@@ -126,9 +131,68 @@ def create_row(data, columns):
         result.append(calculate_diff(1,column,data))
     return result[:]
         
+def find_value(data,position):
+    if position<1 or position>data[-1][0]:
+        return -1
+    if position==data[0][0]:
+        return data[0][1]
+    last=data[-1][1]
+    for c,v in data[::-1]:
+        ##print(c,v)
+        if c<position:
+            return last
+        last=v
+    return data[0][1]
+    
+        
+def count_new_value(data, columns,position):
+    SHIFTS_VERT=[-columns,columns]
+    SHIFTS_LEFT=[-columns-1,-1,columns-1]
+    SHIFTS_RIGHT=[-columns+1,1,columns+1]
+    SHIFTS=SHIFTS_VERT
+    if (position-1)%columns!=0 and position>0:
+        #print('left allowed')
+        SHIFTS.extend(SHIFTS_LEFT)
+    if position%columns!=0 and position>0:
+        #print('right allowed')
+        SHIFTS.extend(SHIFTS_RIGHT)
+    ##print(f'SHIFTS: {SHIFTS}')
+    result=0
+    base=find_value(data,position)
+    #print(f'base: {base}')
+    for shift in SHIFTS:
+        try:
+            value = find_value(data, position+shift)
+            if value != -1:                          
+                new_value=abs(value-base)
+                result=max(result,new_value)
+                #print(value,base,new_value)
+        except IndexError:
+            continue
+    return result
+
+def encode(columns,data):
+    end=data[-1][0]
+    counter=1
+    result=[(-10,0)]
+    while counter<=end:
+        new_value=count_new_value(data,columns,counter)
+        #print(f'counter {counter}, value: {new_value}')
+        if new_value!=result[-1][0]:
+            result.append((new_value,1))#result[-1][1]+1))
+        else:
+            result[-1]=(new_value,result[-1][1]+1)
+        counter+=1
+    return str(columns)+' '+' '.join(str(y) for x in result[1:]for y in x).rstrip()
 
 
+    
 print(decode2(raw2))
+#print(edge_detection(raw) =='7 85 5 0 2 85 5 75 10 150 2 75 3 0 2 150 2 0 4')
 
-print(detect_zero((38,500), 7))
+
+
+
+
+
 
